ce418c10b64f790efeb2eef6bcac0299735635dd
SAMBIT01 <sambitswarup01@gmail.com>
2021-06-16 20:57:32 +0530

    project

--- myFirstAngularProjest/src/app/authentication/login/login.component.ts
+++ myFirstAngularProjest/src/app/authentication/login/login.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { noop } from 'rxjs';
-
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+selector: 'app-login',
+templateUrl: './login.component.html',
+styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+constructor() { }
+ngOnInit(): void {
+}
+showMessageParam = false;
+myName='Your name is:';
+myAddress='Your address is:'
+students= [{rollNo: 1, name: 'Sambit'},
+{rollNo: 2, name: 'Avilipsa'},
+{rollNo: 3, name: 'Abhinav'}]
 
-  constructor() { }
+users ={name:'Sambit',dist:'Nayagarh'}
 
-  ngOnInit(): void {
-  }
-  value = 2;
-  showMessageParam = false;
- 
-  
-  students= [{rollNo: 1, name: 'sambit'},
-              {rollNo: 2, name: 'avilipsa'},
-              {rollNo: 3, name: 'abhinav'}]
-
-              
-
-
-
-  showMessage(){
-    this.showMessageParam = true;
-    // console.log("cancel button works"); 
-    // alert("cancel button works");
-  }
+value =0;
+myColourName='green';
+myBgColour ='white';
+myChoice = 'black';
+classname1='colourCode';
+classname2='cancelbtn';
+showMessage(myTextValue:any){
+this.myColourName='red';
+this.myBgColour ='blue';
+this.myChoice ='grey';
+this.showMessageParam = true;
+this.value =myTextValue;
+// console.log("cancel button works"); 
+// alert("cancel button works");
+}
 }
